6965ace091b556fd76bb090d2ca80811c662cf6c
gopye <gopye@tuta.io>
2019-09-28 19:25:10 -0700

    patch CSRF vulnerability

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,12 @@ class ApplicationController < ActionController::Base
   # Our security guy keep talking about sea-surfing, cool story bro.
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  #protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
+
+  rescue_from ActionController::InvalidAuthenticityToken do |exception|
+    session.clear
+    redirect_to root_path
+  end
 
   private
 
